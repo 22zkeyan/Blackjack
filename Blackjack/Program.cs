caea6c7fac3fdd43b4632fc39fde5c966e2526b5
22zkeyan <22ZKeyan@heckgrammar.co.uk>
2025-11-16 19:49:13 +0000

    coded the totals calculator, works perfectly

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -20,27 +20,59 @@ namespace Blackjack
             Thread.Sleep(2000); //2 seconds of sleep
 
             Random r = new Random();
-            string[,] names_cards = new string[names.Length, 3];
+            string[,] names_cards = new string[names.Length, 2];
             string[] possibleCards = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen" };
             for (int i = 0; i < names_cards.GetLength(0); i++) //loops through every name in the array
             {
-                names_cards[i, 0] = names[i];
-                //this loop starts at 1 to avoid out of index errors or whatever its called
-                for (int j = 1; j < names_cards.GetLength(1); j++) //loops through each location for every name, each person gets two cards
+                for (int j = 0; j < names_cards.GetLength(1); j++) //loops through each location for every name, each person gets two cards
                 {                    
                     names_cards[i, j] = possibleCards[r.Next(0, possibleCards.Length)];
                 }
-                Console.WriteLine($"{names[i]}: {names_cards[i, 1]} and {names_cards[i, 2]}"); //outputs what cards each player has
+                Console.WriteLine($"{names[i]}: {names_cards[i, 0]} and {names_cards[i, 1]}"); //outputs what cards each player has
             }
             return names_cards;
+        }
+
+        static int[] CalcTotal(string[,] names_cards, string[] names)
+        {
+            int[] totals = new int[names_cards.GetLength(0)];
+            int temp = 0; //temporary variable that will store the value of the current card
+            for (int i = 0; i < names_cards.GetLength(0); i++) //loops through every name in the array
+            {
+                //this loop starts at 1 to avoid out of index errors or whatever its called
+                for (int j = 0; j < names_cards.GetLength(1); j++)
+                {
+                    bool success = int.TryParse(names_cards[i, j], out temp);
+                    if (!success)
+                    {
+                        if (names_cards[i, j] == "Ace")
+                        {
+                            Console.WriteLine($"{names[i]} has an ace in their cards, what value do they want it to be? (1 or 11)");
+                            temp = Convert.ToInt32(Console.ReadLine()!);
+                        }
+                        else if (names_cards[i, j] == "Jack" || names_cards[i, j] == "King" || names_cards[i, j] == "Queen")
+                        {
+                            temp = 10;
+                        }                        
+                    }
+                    totals[i] += temp;
+                }                
+            }
+            return totals;
+        }
+
+        static void StandHit()
+        {
+
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Blackjack! Here are the rules:" + 
                 "\n\t- Each person starts out with two cards." + 
-                "\n\t- On your turn, you can choose to either take another card, or hold onto what you have." +
-                "\n\t- The aim of the game is to get the total value of your cards to be 21, or as close to that as possible." + 
+                "\n\t- On your turn, you can choose to either take another card, or 'hit', or 'stand', where you don't take anymore cards." +
+                "\n\t- The aim of the game is to get the total value of your cards to be 21, or as close to that as possible." +
+                "\n\t- You can take as many cards as you wish until you feel you are close enough to 21, in which case you 'hold', or until you exceed 21" + 
                 "\n\t- If the total value of your cards exceeds 21, you are out." + 
                 "\n\t- An ace can be either be worth a 1 or 11, you choose what it's worth");
 
@@ -50,6 +82,12 @@ namespace Blackjack
             Console.WriteLine("How many people are playing today?");
             string[] names = InitPlayers(Convert.ToInt32(Console.ReadLine()!));
             string[,] names_cards = InitCards(names);
+            int[] totals = CalcTotal(names_cards, names);
+            Console.WriteLine("\nCalculating totals...\n");
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine($"{names[i]} total is {totals[i]}");
+            }
         }
     }
 }
